ea72be0d40e0cdfaac5584ef83ce15f0d07a2c7b
SerhiiVakoliuk <s.vakoliuk@gmail.com>
2021-06-03 17:40:44 +0300

    03 june 2021

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,33 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import {HttpClientModule} from "@angular/common/http";
+import {RouterModule} from "@angular/router";
+import { PostComponent } from './post/post.component';
+import { PostsComponent } from './posts/posts.component';
+import { HomeComponent } from './home/home.component';
+import { CommentsComponent } from './comments/comments.component';
+import { PostDetailsComponent } from './post-details/post-details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PostComponent,
+    PostsComponent,
+    HomeComponent,
+    CommentsComponent,
+    PostDetailsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    RouterModule.forRoot([
+      {path: 'home', component:HomeComponent},
+      {path: 'posts', component:PostComponent, children: [
+          {path: ':id', component:PostDetailsComponent}
+        ]},
+      {path: 'comments', component:CommentsComponent}
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
